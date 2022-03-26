bbbbf775a39f005422a721126dc095e782b0a9db
Ильшат Баширов <i.bashirov@quality-lab.ru>
2022-03-27 02:40:17 +0300

    В ContactInformationTest реализвана проверка email. Так же реализован тест для посика контактов ContactSearch

--- addressbook-web-tests/addressbook-web-tests/ContactInformationTests.cs
+++ addressbook-web-tests/addressbook-web-tests/ContactInformationTests.cs
@@ -17,6 +17,10 @@ namespace WebAddressbookTests
             Assert.AreEqual(fromTable,fromForm);
             Assert.AreEqual(fromTable.Address, fromForm.Address);
             Assert.AreEqual(fromTable.AllPhones, fromForm.AllPhones);
+
+            Assert.AreEqual(fromTable.AllEmail, fromForm.AllEmail);
+
+
         }
 
     }
